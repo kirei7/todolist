cdcb2db8ff1b45cc48f9a5c47279010be7f88f28
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-28 11:20:55 +0300

    Add logging

--- src/main/java/com/epam/rd/todolist/App.java
+++ src/main/java/com/epam/rd/todolist/App.java
@@ -3,6 +3,8 @@ package com.epam.rd.todolist;
 import com.epam.rd.todolist.spark.SparkConfig;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -11,8 +13,10 @@ import com.google.inject.Injector;
  */
 public class App 
 {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args )
     {
+        logger.info("Starting application");
         Injector injector = Guice.createInjector();
 
         SparkConfig spark = injector.getInstance(SparkConfig.class);
