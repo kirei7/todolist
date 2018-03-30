5f69c1250829d8eb94365b9f223b509ddfd41136
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-30 10:41:24 +0300

    Change port

--- src/main/java/com/epam/rd/todolist/config/SparkConfig.java
+++ src/main/java/com/epam/rd/todolist/config/SparkConfig.java
@@ -1,10 +1,12 @@
 package com.epam.rd.todolist.config;
 
+import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 public class SparkConfig {
     public SparkConfig() {
         staticFiles.location("/static");
+        port(8080);
         /*String path = System.getProperty("user.dir") + "/src/main/resources/static";
         staticFiles.externalLocation(path);*/
     }
