8bda0a811a6ea90e11a933a3f29eba3ae90e099a
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-30 10:54:12 +0300

    Port settings dynamic

--- src/main/java/com/epam/rd/todolist/config/SparkConfig.java
+++ src/main/java/com/epam/rd/todolist/config/SparkConfig.java
@@ -1,9 +1,13 @@
 package com.epam.rd.todolist.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 public class SparkConfig {
+    private final Logger logger = LoggerFactory.getLogger(SparkConfig.class);
     public SparkConfig() {
         staticFiles.location("/static");
         String portStr = System.getenv("PORT");
