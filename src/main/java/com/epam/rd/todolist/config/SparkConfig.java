19d2179685aaa2e345ec9ca8f757ad5c5273bc65
Vladyslav Sereda <Vladyslav_Sereda1@epam.com>
2018-03-30 10:53:12 +0300

    Port settings dynamic

--- src/main/java/com/epam/rd/todolist/config/SparkConfig.java
+++ src/main/java/com/epam/rd/todolist/config/SparkConfig.java
@@ -6,7 +6,14 @@ import static spark.Spark.staticFiles;
 public class SparkConfig {
     public SparkConfig() {
         staticFiles.location("/static");
-        port(8080);
+        String portStr = System.getenv("PORT");
+        int port;
+        try {
+            port = Integer.parseInt(portStr);
+        } catch (Exception ex) {
+            port = 8080;
+        }
+        port(port);
         /*String path = System.getProperty("user.dir") + "/src/main/resources/static";
         staticFiles.externalLocation(path);*/
     }
